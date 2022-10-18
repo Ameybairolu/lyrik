030536510a325acf800e492c6b70a1ebb8e4dd90
Ameybairolu <ameybairolu@gmail.com>
2022-10-18 16:05:10 +0530

    Create Loader component to display loader when fetching data

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,16 @@
-const Loader: React.FC = () => {
-    return <div>Loader</div>;
+import { loader } from "assets";
+
+interface LoaderProps {
+    title?: string;
+}
+
+const Loader: React.FC<LoaderProps> = ({ title }) => {
+    return (
+        <div className="w-full flex justify-center items-center flex-col">
+            <img src={loader} alt="loader" className="w-32 h-32 object-contain" />
+            <h1 className="font-bold text-2xl text-white mt-2">{title ?? "Loading..."}</h1>
+        </div>
+    );
 };
 
 export default Loader;
